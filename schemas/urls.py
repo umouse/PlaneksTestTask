70530a58722e3e5c580ce0846183820f60527ca9
Julia Ishchenko <umousee@gmail.com>
2020-12-21 18:03:15 +0200

    Update delete and edit buttons to a working state. add  new schema button to the schema list view

--- schemas/urls.py
+++ schemas/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('create_schema/', views.SchemaCreateView.as_view(), name='SchemaCreateView'),
     path('schemas_list/', views.SchemaListView.as_view(), name='SchemaListView'),
+    path('update_schema/<int:pk>/', views.SchemaUpdateView.as_view(), name='SchemaUpdateView'),
+    path('delete_schema/<int:pk>/', views.SchemaDeleteView.as_view(), name='SchemaDeleteView'),
 ]
--- schemas/views.py
+++ schemas/views.py
@@ -3,8 +3,14 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.forms.models import inlineformset_factory
-from django.views.generic import CreateView, ListView
+from django.views.generic import CreateView, ListView, UpdateView, DeleteView
 from schemas.models import Schema, Column
+
+column_formset = inlineformset_factory(
+            Schema, Column, fields=('name', 'column_types', 'order'),
+            labels={'name': 'Column name', 'column_types': 'Type', 'order': 'Order'},
+            can_order=False, can_delete=False
+        )
 
 
 class SchemaListView(ListView):
@@ -24,11 +30,6 @@ class SchemaCreateView(LoginRequiredMixin, CreateView):
 
     def get_context_data(self, **kwargs):
         data = super().get_context_data(**kwargs)
-        column_formset = inlineformset_factory(
-            Schema, Column, fields=('name', 'column_types', 'order'),
-            labels={'name': 'Column name', 'column_types': 'Type', 'order': 'Order'},
-            can_order=False, can_delete=False
-        )
         helper = ColumnFormSetHelper()
         data["helper"] = helper
         if self.request.POST:
@@ -48,6 +49,34 @@ class SchemaCreateView(LoginRequiredMixin, CreateView):
         return super().form_valid(form)
 
 
+class SchemaUpdateView(LoginRequiredMixin, UpdateView):
+    model = Schema
+    fields = ['name', 'column_separator', 'string_character']
+    success_url = reverse_lazy('SchemaListView')
 
+    def get_context_data(self, **kwargs):
+        data = super().get_context_data(**kwargs)
+        helper = ColumnFormSetHelper()
+        data["helper"] = helper
+        if self.request.POST:
+            data["columns"] = column_formset(self.request.POST, instance=self.object)
+        else:
+            data["columns"] = column_formset(instance=self.object)
+        return data
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        context = self.get_context_data()
+        columns = context["columns"]
+        self.object = form.save()
+        if columns.is_valid():
+            columns.instance = self.object
+            columns.save()
+        return super().form_valid(form)
+
+
+class SchemaDeleteView(DeleteView):
+    model = Schema
+    success_url = reverse_lazy('SchemaListView')
 
 
